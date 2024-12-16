bc016a35a399968c516cdf2bfe48552d8a2e056a
Facultad-de-Ingenieria-E-Mochis <facultaddeingenieriaemochis@gmail.com>
2024-12-16 11:44:20 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -6,22 +6,24 @@ const btnclose = document.querySelector('.closebtn i');
 
 var chatbox = document.getElementById('fb-customer-chat');
 chatbox.setAttribute('page_id', '502961932904858'); // Reemplaza con el ID de tu página de Facebook
-chatbox.setAttribute('attribution', 'biz_inbox');
+chatbox.setAttribute('attribution', 'biz_inbox'); // Atribución para el buzón de negocios
 
+// Inicialización del SDK de Facebook
 window.fbAsyncInit = function () {
   FB.init({
     xfbml: true,
-    version: 'v17.0', // Cambia según la versión más reciente de la API
+    version: 'v17.0', // Usa la versión más reciente de la API
   });
 };
 
+// Cargar el SDK de Facebook de forma asíncrona
 (function (d, s, id) {
   var js,
     fjs = d.getElementsByTagName(s)[0];
   if (d.getElementById(id)) return;
   js = d.createElement(s);
   js.id = id;
-  js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+  js.src = 'https://connect.facebook.net/es_ES/sdk/xfbml.customerchat.js';
   fjs.parentNode.insertBefore(js, fjs);
 })(document, 'script', 'facebook-jssdk');
 
